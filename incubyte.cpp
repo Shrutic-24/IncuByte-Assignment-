96a87bb9b097cc7e1746136ede5452b9bea4345d
Shruti Chavan <chavanshruti630@gmail.com>
2023-09-14 12:46:54 +0530

    Created Spacecraft class as basic building block

--- incubyte.cpp
+++ incubyte.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Spacecraft
+{
+private:
+    int x, y, z;
+    char direction;
 
+public:
+    Spacecraft(){
+        x=0;
+        y=0;
+        z=0;
+        direction='E';
+    }
+    Spacecraft(int x0, int y0, int z0, char dir0)
+    {
+        x = x0;
+        y = y0;
+        z = z0;
+        direction = dir0;
+    }
+
+};
 
 int main()
 {
